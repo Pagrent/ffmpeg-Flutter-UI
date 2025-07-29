de8c1fbe62ad51bd64c6a4ad0bd40d64bf81e1fb
Pagrent <lpg01102007@outlook.com>
2025-07-29 22:05:35 +0800

    修改：     ffmpegflutterui/lib/pages/convert_page.dart 	修改：     ffmpegflutterui/lib/pages/input_page.dart 	修改：     ffmpegflutterui/lib/pages/platform_page.dart 	新文件：   ffmpegflutterui/lib/utils/confirm_file.dart 	新文件：   ffmpegflutterui/lib/utils/file_list.dart 	修改：     ffmpegflutterui/pubspec.lock 	修改：     ffmpegflutterui/pubspec.yaml

--- ffmpegflutterui/lib/pages/convert_page.dart
+++ ffmpegflutterui/lib/pages/convert_page.dart
@@ -1,14 +1,28 @@
+import 'package:ffmpegflutterui/utils/confirm_file.dart';
+import 'package:ffmpegflutterui/utils/file_list.dart';
 import 'package:flutter/material.dart';
 
 class ConvertPage extends StatefulWidget {
-  const ConvertPage({super.key});
+  final List<String> fileList;
+
+  const ConvertPage({
+    required this.fileList,
+    super.key,
+  });
 
   @override
   State<ConvertPage> createState() => _ConvertPageState();
 }
 
 class _ConvertPageState extends State<ConvertPage> {
+
+
   @override
+  void initState() {
+  super.initState();
+  print("你就赤石吧: ${widget.fileList.length}");
+  print("赤这么多石: ${widget.fileList.join(', ')}");
+}
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -21,6 +35,18 @@ class _ConvertPageState extends State<ConvertPage> {
         title: Text("Convert"),
         backgroundColor: Colors.deepPurple[100],
       ),
+
+
+      body: ListView.builder(
+        itemCount: widget.fileList.length,
+        itemBuilder: (context, index){
+          return ConfirmFile(
+            fileName: widget.fileList[index],
+          );
+        },
+      ),
+
+      
     );
   }
 }
