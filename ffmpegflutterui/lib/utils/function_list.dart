307752eaa19c6d1ef38b7f45614682380f3698df
Pagrent <lpg01102007@outlook.com>
2025-08-02 21:41:40 +0800

    修改：     ffmpegflutterui/android/app/build.gradle.kts 	修改：     ffmpegflutterui/android/gradle.properties 	修改：     ffmpegflutterui/lib/pages/convert_page.dart 	新文件：   ffmpegflutterui/lib/pages/function_page.dart 	修改：     ffmpegflutterui/lib/pages/input_page.dart 	修改：     ffmpegflutterui/lib/pages/platform_page.dart 	新文件：   ffmpegflutterui/lib/utils/function_list.dart 	新文件：   ffmpegflutterui/lib/utils/function_list_2.dart 	新文件：   ffmpegflutterui/lib/utils/info_list.dart

---
+++ ffmpegflutterui/lib/utils/function_list.dart
@@ -0,0 +1,75 @@
+import 'dart:ffi';
+
+import 'package:ffmpegflutterui/pages/input_page.dart';
+import 'package:ffmpegflutterui/utils/function_list_2.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class FunctionList extends StatefulWidget {
+  
+  FunctionList({
+    super.key,
+    
+  });
+
+  @override
+  State<FunctionList> createState() => _FunctionListState();
+}
+
+class _FunctionListState extends State<FunctionList> {
+  int? selectedCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+        Container(
+          height: 120,
+          width: MediaQuery.of(context).size.width / 2 + 35,
+          decoration: BoxDecoration(
+            color: Colors.deepPurple[100],
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Category",
+                style: TextStyle(
+                  fontSize: 20
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              DropdownMenu(
+                enableFilter: true,
+                width: MediaQuery.of(context).size.width / 2,
+                onSelected: (Category) {
+                  if (Category != null) {
+                    setState(() {
+                      selectedCategory = Category;
+                    });
+                  }
+                },
+                dropdownMenuEntries: <DropdownMenuEntry<int>>[
+                  DropdownMenuEntry(value: 0, label: "All Functions"),
+                  DropdownMenuEntry(value: 1, label: "Video Process"),
+                  DropdownMenuEntry(value: 2, label: "Audio Process"),
+                  DropdownMenuEntry(value: 3, label: "Other Process")
+                ],
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        ShinoList(selectedCategory: selectedCategory)
+      ]
+    );
+  }
+}
---
+++ ffmpegflutterui/lib/utils/function_list_2.dart
@@ -0,0 +1,240 @@
+import 'package:ffmpegflutterui/utils/info_list.dart';
+import 'package:flutter/material.dart';
+
+class ShinoList extends StatefulWidget {
+  final int? selectedCategory;
+
+  ShinoList({
+    super.key,
+    required this.selectedCategory,
+  });
+
+  @override
+  State<ShinoList> createState() => _ShinoListState();
+}
+
+class _ShinoListState extends State<ShinoList> {
+  int? selectedFunction;
+
+  @override
+  Widget build(BuildContext context) {
+    
+    if(widget.selectedCategory == 1) {
+      return Column(
+        children: [
+          Container(
+            height: 120,
+            width: MediaQuery.of(context).size.width / 2 + 35,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple[100],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Video",
+                  style: TextStyle(
+                    fontSize: 20
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                DropdownMenu(
+                  enableFilter: true,
+                  width: MediaQuery.of(context).size.width / 2,
+                  onSelected: (sukiFunction) {
+                    setState(() {
+                      selectedFunction = sukiFunction;
+                    });
+                  },
+                  dropdownMenuEntries: <DropdownMenuEntry<int>>[
+                    DropdownMenuEntry(value: 1, label: "Format Conversion"),
+                    DropdownMenuEntry(value: 2, label: "Video Transcoding"),
+                    DropdownMenuEntry(value: 3, label: "Trim Video"),
+                    DropdownMenuEntry(value: 4, label: "Adjust Resolution"),
+                    DropdownMenuEntry(value: 5, label: "Adjust Framerate"),
+                    DropdownMenuEntry(value: 6, label: "Rotate Video"),
+                    DropdownMenuEntry(value: 7, label: "Remove Audio Track"),
+                    DropdownMenuEntry(value: 8, label: "Extract Audio Track"),
+                    DropdownMenuEntry(value: 9, label: "Video Screenshot"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          InfoList(
+            selectedFunction: selectedFunction,
+          ),
+        ],
+      );
+    }
+    if(widget.selectedCategory == 2) {
+      return Column(
+        children: [
+          Container(
+            height: 120,
+            width: MediaQuery.of(context).size.width / 2 + 35,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple[100],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Audio",
+                  style: TextStyle(
+                    fontSize: 20
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                DropdownMenu(
+                  enableFilter: true,
+                  width: MediaQuery.of(context).size.width / 2,
+                  onSelected: (sukiFunction) {
+                    setState(() {
+                      selectedFunction = sukiFunction;
+                    });
+                  },
+                  dropdownMenuEntries: <DropdownMenuEntry<int>>[
+                    DropdownMenuEntry(value: 21, label: "Format Convertion"),
+                    DropdownMenuEntry(value: 22, label: "Audio Transcoding"),
+                    DropdownMenuEntry(value: 23, label: "Adjust Volume"),
+                    DropdownMenuEntry(value: 24, label: "Trim Audio"),
+                    DropdownMenuEntry(value: 25, label: "Convert Sample Rate"),
+                    DropdownMenuEntry(value: 26, label: "Adjust Channel Count"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          InfoList(
+            selectedFunction: selectedFunction,
+          ),
+        ],
+      );
+    }
+    if(widget.selectedCategory == 3) {
+      return Column(
+        children: [
+          Container(
+            height: 120,
+            width: MediaQuery.of(context).size.width / 2 + 35,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple[100],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Other",
+                  style: TextStyle(
+                    fontSize: 20
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                DropdownMenu(
+                  enableFilter: true,
+                  width: MediaQuery.of(context).size.width / 2,
+                  onSelected: (sukiFunction) {
+                    setState(() {
+                      selectedFunction = sukiFunction;
+                    });
+                  },
+                  dropdownMenuEntries: <DropdownMenuEntry<int>>[
+                    DropdownMenuEntry(value: 31, label: "Video to GIF"),
+                    DropdownMenuEntry(value: 32, label: "Extract Video Frames"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          InfoList(
+            selectedFunction: selectedFunction,
+          ),
+        ],
+      );
+    }
+
+    return Column(
+      children: [
+        Container(
+          height: 120,
+          width: MediaQuery.of(context).size.width / 2 + 35,
+          decoration: BoxDecoration(
+            color: Colors.deepPurple[100],
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Function",
+                style: TextStyle(
+                  fontSize: 20
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              DropdownMenu(
+                enableFilter: true,
+                width: MediaQuery.of(context).size.width / 2,
+                dropdownMenuEntries: <DropdownMenuEntry<int>>[
+                  DropdownMenuEntry(value: 1, label: "Format Conversion"),
+                  DropdownMenuEntry(value: 2, label: "Video Transcoding"),
+                  DropdownMenuEntry(value: 3, label: "Trim Video"),
+                  DropdownMenuEntry(value: 4, label: "Adjust Resolution"),
+                  DropdownMenuEntry(value: 5, label: "Adjust Framerate"),
+                  DropdownMenuEntry(value: 6, label: "Rotate Video"),
+                  DropdownMenuEntry(value: 7, label: "Remove Audio Track"),
+                  DropdownMenuEntry(value: 8, label: "Extract Audio Track"),
+                  DropdownMenuEntry(value: 9, label: "Video Screenshot"),
+              
+                  DropdownMenuEntry(value: 21, label: "Format Convertion"),
+                  DropdownMenuEntry(value: 22, label: "Audio Transcoding"),
+                  DropdownMenuEntry(value: 23, label: "Adjust Volume"),
+                  DropdownMenuEntry(value: 24, label: "Trim Audio"),
+                  DropdownMenuEntry(value: 25, label: "Convert Sample Rate"),
+                  DropdownMenuEntry(value: 26, label: "Adjust Channel Count"),
+              
+                  DropdownMenuEntry(value: 31, label: "Video to GIF"),
+                  DropdownMenuEntry(value: 32, label: "Extract Video Frames"),
+                ],
+              ),
+            ],
+          ),
+        ),
+        InfoList(
+          selectedFunction: selectedFunction,
+        ),
+      ],
+    );
+
+    
+  }
+}
